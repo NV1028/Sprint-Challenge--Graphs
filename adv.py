9a0c9a0d57350120a40e1bd5d8736e8c3dfeb289
Nick Verdin <nv1028@aggienetwork.com>
2020-09-01 19:07:12 -0500

    mvp

--- adv.py
+++ adv.py
@@ -22,20 +22,89 @@ world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
-
+print(world)
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+
 traversal_path = []
 
-
+# test data below
+# the numbers in parentheses are coordinates 
+# the dictionary to the right of each coordinate 
+# is which direction you can go and which room you 
+# will be in if you go that direction
+# {
+#   0: [(3, 5), {'n': 1}],
+#   1: [(3, 6), {'s': 0, 'n': 2}],
+#   2: [(3, 7), {'s': 1}]
+# }
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
+print("player.current_room:", player.current_room)
 visited_rooms.add(player.current_room)
 
+#ADDING MY CODE UNDER HERE BECAUSE I NEED TO MAKE USE OF VISITED_ROOMS
+
+# --------------------------------------------------------------------
+#dictionary for steps forward
+# moving_forward = {
+#     'n': 'e',
+#     'e': 's',
+#     's': 'w',
+#     'w': None
+# }
+#dictionary for steps backwards
+back_it_up = {
+    'n': 's',
+    's': 'n',
+    'e': 'w',
+    'w': 'e'
+}
+#create a path stack
+path = ['n']
+#while the length of the stack is greater than 0
+while len(path) > 0:
+    #assign a variable to pop off the stack
+    moving = path.pop()
+    #now I want to call player.travel passing in the variable I just created 
+    player.travel(moving)
+    
+    # now if player.current room has not been visited
+    # in other words if you have discovered a new room
+    if player.current_room not in visited_rooms:
+        #append to traversal_path the dictionary for the steps to go back with the variable we made above as the indicies
+        traversal_path.append(back_it_up[moving])
+        # print("traversal_path:", traversal_path)
+        print("back_it_up[moving]:", back_it_up[moving])
+        print("moving:", moving)
+        # also append it to our path stack
+        # this is our path back
+        path.append(back_it_up[moving])
+        print("path (stack):", path)
+        # and add the current room to our visited rooms
+        visited_rooms.add(player.current_room)
+        print("length of visited rooms:", len(visited_rooms))
+        # print("length of player.current_room.get_exits:", len(player.current_room.get_exits_string))
+    
+    # for our new_direction in [n,s,e,w]
+    for new_direction in ['n', 's', 'e', 'w']:
+        #we want to go to a new room, by calling get_room_in_directions(new_direction)
+        next_room = player.current_room.get_room_in_direction(new_direction)
+        #if new room exists and is not in visited
+        print("next_room:", next_room)
+        if next_room and next_room not in visited_rooms:
+            #append our new direction to traversal_path
+            traversal_path.append(new_direction)
+            #and also append it to our path stack
+            path.append(new_direction)
+            #time to break
+            break
+# ---------------------------------------------------------------------
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
@@ -48,15 +117,17 @@ else:
 
 
 
+
+
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
